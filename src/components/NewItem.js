b49f2ffc1495fa9dd9a2c61f4bb8e5d3da75b0d1
Mohit Maroliya <mmaroliya@gmail.com>
2021-09-11 17:25:05 +0530

    Initial Commit

---
+++ src/components/NewItem.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const NewItem = (props) => {
+
+    let { title, description, imageUrl, newsUrl, author, publishedAt, source } = props;
+
+    return (
+      <div className="my-3">
+        <div className="card" style={{border:'0.1px groove #808183'}} >
+          <div style={{ display:'flex',alignItems:'flex-end',right:'0',position:'absolute' }}>
+            <span
+              className="badge rounded-pill bg-primary"
+            >
+              {source}
+            </span>
+          </div>
+         
+          <img src={imageUrl} className="card-img-top" style={{height:'232.75px'}} alt="..." />
+          <div className="card-body" style={{border:'0.5px inset #808183'}}>
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {author} at {new Date(publishedAt).toGMTString()}
+              </small>
+            </p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-dark btn-sm"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  
+}
+
+export default NewItem;
+
---
+++ src/components/News.js
@@ -0,0 +1,110 @@
+import React, { useState,useEffect } from "react";
+import NewItem from "./NewItem";
+import Spinner from "./Spinner";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const News = (props) => {
+
+
+  const capitilizeFirstLetter = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0)
+
+
+
+  
+
+  const updateNews = async() => {
+    props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apikey}&page=${page}&pageSize=${props.pageSize}`;
+    setLoading(true);
+    let data = await fetch(url);
+    props.setProgress(30);
+    let parsedData = await data.json();
+    props.setProgress(50);
+    //console.log(parsedData)
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
+    props.setProgress(100);
+  }
+
+
+  useEffect(() => {
+    updateNews();
+    document.title = `NewsReader2 - ${capitilizeFirstLetter(props.category)}`;
+  }, [])
+
+
+
+  const fetchMoreData = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apikey}&page=${page+1}&pageSize=${props.pageSize}`;
+    setPage(page+1);
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    //console.log(parsedData)
+    setArticles(articles.concat(parsedData.articles));
+    setTotalResults(parsedData.totalResults);
+  };
+
+    return (
+      <>
+        <h1 className="text-center" style={{ margin: "35px 0px", marginTop:'5%' }}>
+          New Reader2 - By Mohit Maroliya
+        </h1>
+
+        <div
+          className="alert alert-success text-center"
+          role="alert"
+          style={{ fontSize: "150%", fontWeight: 700 }}
+        >
+          News from {capitilizeFirstLetter(props.category)} world
+        </div>
+
+        {loading && <Spinner />}
+
+        <InfiniteScroll
+          next={fetchMoreData}
+          dataLength={
+            articles.length ? articles.length : 15
+          }
+          hasMore={articles.length !== totalResults}
+          loader={<Spinner />}
+        >
+          <div className="container">
+            <div className="row">
+              {articles.map((item) => {
+                return (
+                  <div className="col-md-4" key={item.url}>
+                    <NewItem
+                      title={item.title ? item.title.slice(0, 45) : ""}
+                      description={
+                        item.description ? item.description.slice(0, 88) : ""
+                      }
+                      imageUrl={
+                        item.urlToImage
+                          ? item.urlToImage
+                          : "https://www.eastmojo.com/wp-content/uploads/2021/08/space-pen-5.jpg"
+                      }
+                      newsUrl={item.url}
+                      publishedAt={item.publishedAt}
+                      author={item.author ? item.author : "Anonymous"}
+                      source={item.source.name}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
+      </>
+    );
+  
+}
+
+export default News;
