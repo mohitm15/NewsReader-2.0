5697442a6b5cc349455293f4a813642f70f3f3e3
Mohit Maroliya <mmaroliya@gmail.com>
2022-03-30 18:34:21 +0530

    Minor changes-2

--- src/components/News.js
+++ src/components/News.js
@@ -22,7 +22,7 @@ const News = (props) => {
 
   const updateNews = async() => {
     props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apikey}&page=${page}&pageSize=${props.pageSize || "12"}`;
+    const url = `https://saurav.tech/NewsAPI/top-headlines/category/${props.category}/${props.country}.json`;
     setLoading(true);
     let data = await fetch(url);
     props.setProgress(30);
